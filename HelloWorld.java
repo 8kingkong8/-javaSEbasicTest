019e4c65fa6bc3afae5429c38a62e80027536109
8kingkong8 <2841266208@qq.com>
2019-08-12 11:10:50 +0800

    third commit

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld{
+    public static void main(){
+	    System.out.print("helloworld");
+		/*
+	public static void main(String[] args){
+        int x = 3;
+        int y = 5;
+		int z = getSum(x,y);
+		System.out.println(getSum(10,9) + z);
+    }
+    public static int getSum(int a,int b){
+			int result = a + b;
+			return  result;
+	}
+	*/
+	}
+}
+	
